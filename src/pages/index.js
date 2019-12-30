2ec2fdbac9826f7e9b994604ec3717fca8b6a39d
Udit Desai <udit.desai3@gmail.com>
2019-12-30 11:59:16 -0500

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,82 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+const IndexPage = () => {
+  const [name, setName] = useState("")
+  const [canAttend, setCanAttend] = useState("")
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+  const nameHandler = e => {
+    setName(e.target.value)
+  }
+
+  const canAttendHandler = e => {
+    setCanAttend(e.target.value)
+  }
+
+  const submitInfo = e => {
+    e.preventDefault()
+
+    const data = {
+      guest: {
+        name: name,
+        canAttend: canAttend,
+      },
+    }
+
+    console.log(data)
+
+    fetch(
+      "https://v2-api.sheety.co/efd2a35bf372a3cbe5976ef9c8e084d8/kathanGetsRich/guests",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
+    )
+      .then(response => response.json())
+      .then(data => {
+        console.log("Success:", data)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
+
+  useEffect(() => {
+    fetch(
+      "https://v2-api.sheety.co/efd2a35bf372a3cbe5976ef9c8e084d8/kathanGetsRich/guests"
+    )
+      .then(results => {
+        return results.json()
+      })
+      .then(data => {
+        console.log(data)
+      })
+  }, [])
+
+  return (
+    <div>
+      <h1>Kathan Gets Rich</h1>
+
+      <form>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" value={name} onChange={nameHandler} />
+
+        <label htmlFor="canAttend">Can you attend? (Yes or No)</label>
+        <input
+          type="text"
+          id="canAttend"
+          value={canAttend}
+          onChange={canAttendHandler}
+        />
+
+        <button onClick={submitInfo}>Submit</button>
+      </form>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+  )
+}
 
 export default IndexPage
